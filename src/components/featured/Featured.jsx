4d4a01fd92b1d8e5e6ab9eb68cd34d2403ab4e8e
Avik Mallick <avikmallick111101@gmail.com>
2023-12-22 00:54:33 +0530

    till 54:32

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,6 +1,12 @@
 import styles from './featured.module.css';
 
 const Featured = () => {
-	return <div className={styles.container}>Featured</div>;
+	return (
+		<div className={styles.container}>
+			<h1 className={styles.title}>
+				<b>Hey Avik here!</b> Discover my stories and creative ideas
+			</h1>
+		</div>
+	);
 };
 export default Featured;
